f56820fd35d3eed7e36c4a1b797d03b5031b7251
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-01-01 21:04:02 +0100

    finished signIn backend and SignInScreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id ("kotlin-kapt")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -59,6 +61,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.google.firebase:firebase-auth:22.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -66,4 +69,12 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation ("com.google.firebase:firebase-auth-ktx:21.1.0")
+    implementation ("com.google.android.gms:play-services-auth:20.4.1")
+
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.0")
+    implementation ("androidx.lifecycle:lifecycle-runtime-compose:2.6.0")
+    implementation ("androidx.navigation:navigation-compose:2.7.6")
+    implementation ("io.coil-kt:coil-compose:2.2.2")
 }
---
+++ app/src/main/java/com/example/firebasegooglesignin/presentation/sign_in/SignInState.kt
@@ -0,0 +1,8 @@
+package com.example.firebasegooglesignin.presentation.sign_in
+
+import java.lang.Error
+
+data class SignInState(
+    val isSignSuccessful: Boolean = false,
+    val signInError: String? = null
+)
